bb2640d846a4b205f5f17c19bde76f20444bbe59
GuinGura <yumanagura@gmail.com>
2025-03-13 15:42:40 +0900

    src/main/resources/static/js/stripe.jsを修正

--- src/main/resources/static/js/stripe.js
+++ src/main/resources/static/js/stripe.js
@@ -1,4 +1,4 @@
-const stripe = Stripe(pk_test_51Qs0EWQ57R136RDNITjiCNgfgiZgShIPb3WT8P2xYRRCDy4JL7PAUVAyavtrUFQjhWBkOWZa76gUXYK590ERDxor00vAQGEimg);
+const stripe = Stripe('pk_test_51Qs0EWQ57R136RDNITjiCNgfgiZgShIPb3WT8P2xYRRCDy4JL7PAUVAyavtrUFQjhWBkOWZa76gUXYK590ERDxor00vAQGEimg');
 const paymentButton = document.querySelector('#paymentButton');
 
 paymentButton.addEventListener('click', () => {
